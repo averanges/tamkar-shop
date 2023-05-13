960a3d23929e46de80bab9963614dc1166b98b31
averanges <nick.belichenko@gmail.com>
2023-05-13 19:20:25 +0900

    Update paths 9

--- client/src/services/apiQuery.jsx
+++ client/src/services/apiQuery.jsx
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiQuery = createApi({
     reducerPath: 'apiQuery',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://neon-halva-0a8681.netlify.app' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://tamkar-backend.netlify.app' }),
     endpoints: () => ({}),
   });

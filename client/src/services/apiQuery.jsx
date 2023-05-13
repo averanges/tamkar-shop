8ab6c3d20f8453171634f0dda508956a5e7d3e85
averanges <nick.belichenko@gmail.com>
2023-05-13 17:38:54 +0900

    Update paths 7

--- client/src/services/apiQuery.jsx
+++ client/src/services/apiQuery.jsx
@@ -3,9 +3,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiQuery = createApi({
     reducerPath: 'apiQuery',
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.VERCEL_URL,
-        credentials: 'include',
-        mode: 'cors'
+      baseUrl: 'https://tamkar-shop.vercel.app',
+      prepareHeaders: (headers) => {
+        headers.set('Access-Control-Allow-Credentials', 'true');
+        headers.set('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+        return headers;
+      },
     }),
-    endpoints: () => ({})
-})
+    endpoints: () => ({}),
+  });

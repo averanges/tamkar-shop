547b3d49d3b5ef0fdac12e2bff1dfd39bf03b42c
averanges <nick.belichenko@gmail.com>
2023-05-13 16:44:23 +0900

    Updated paths 5

--- client/src/services/apiQuery.jsx
+++ client/src/services/apiQuery.jsx
@@ -2,7 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiQuery = createApi({
     reducerPath: 'apiQuery',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://tamkar-shop.vercel.app'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://tamkar-shop.vercel.app',
+        credentials: 'include',
+        mode: 'cors'
+    }),
     endpoints: () => ({})
 })
-

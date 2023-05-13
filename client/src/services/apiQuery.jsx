338a73e48e4c73525f072fe854983ba6b82e4294
averanges <nick.belichenko@gmail.com>
2023-05-13 16:28:51 +0900

    Updated for deployement paths

--- client/src/services/apiQuery.jsx
+++ client/src/services/apiQuery.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiQuery = createApi({
     reducerPath: 'apiQuery',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://tamkar-beauty-backend.onrender.com'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://tamkar-shop.vercel.app/'}),
     endpoints: () => ({})
 })
 

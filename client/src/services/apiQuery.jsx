d251d592768b6a72f7f9794d1c00d8cd4f28bf1a
averanges <nick.belichenko@gmail.com>
2023-05-13 19:15:11 +0900

    Update paths 8

--- client/src/services/apiQuery.jsx
+++ client/src/services/apiQuery.jsx
@@ -2,13 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiQuery = createApi({
     reducerPath: 'apiQuery',
-    baseQuery: fetchBaseQuery({
-      baseUrl: 'https://tamkar-shop.vercel.app',
-      prepareHeaders: (headers) => {
-        headers.set('Access-Control-Allow-Credentials', 'true');
-        headers.set('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-        return headers;
-      },
-    }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://neon-halva-0a8681.netlify.app' }),
     endpoints: () => ({}),
   });
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(passport.initialize())
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'https://tamkar-beauty.netlify.app',
     credentials: true
   }));
 app.use(express.json())

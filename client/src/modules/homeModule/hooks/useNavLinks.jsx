1dc51204f1a23f148e4f8bdf2a1eebbb5fcfd1de
averanges <nick.belichenko@gmail.com>
2023-05-14 13:48:14 +0900

    Update mobile version 2

--- client/src/modules/homeModule/hooks/useNavLinks.jsx
+++ client/src/modules/homeModule/hooks/useNavLinks.jsx
@@ -1,8 +1,7 @@
 import NavLinkText from '../subComponents/NavLinkText'
 
 export const useNavLinks = () => {
-const navArr = [{name: 'Hair Care', link: '/shop/hair-care'}, {name: 'Makeup', link: '/shop/makeup'}, {name: 'Skin Care', link: '/shop/skin-care'}, 
-{name: 'Body Care', link: '/shop/body-care'}, {name: 'Sales', link: '/shop/sales'}, {name: 'New', link: '/shop/new'}]
+const navArr = [{name: 'Hair Care', link: '/shop/hair-care'}, {name: 'Sales', link: '/shop/sales'}, {name: 'New', link: '/shop/new'}]
 .map((el,idx) => <NavLinkText key={idx} link={el.link} name={el.name}/>)
 return navArr
 }

a52166f553f8ac77a24b0849e658512bdfe0343b
averanges <nick.belichenko@gmail.com>
2023-05-15 16:12:09 +0900

    Small updates

--- client/src/services/apiQuery.jsx
+++ client/src/services/apiQuery.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiQuery = createApi({
     reducerPath: 'apiQuery',
     baseQuery: fetchBaseQuery({ 
-       baseUrl: 'https://tamkar-beauty-backend.onrender.com'
+       baseUrl: 'https://tamkar-server.onrender.com'
      }),
     endpoints: () => ({}),
   });
